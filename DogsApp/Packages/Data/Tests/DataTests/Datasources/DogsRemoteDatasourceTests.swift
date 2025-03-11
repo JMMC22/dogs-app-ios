5f52eb92b888624d2c2035764dfc932a5eecae22
José María Márquez <jmmarquezcrespo@gmail.com>
2025-03-11 18:26:44 +0100

    test: add dogs remote datasource tests

--- DogsApp/Packages/Data/Tests/DataTests/Datasources/DogsRemoteDatasourceTests.swift
+++ DogsApp/Packages/Data/Tests/DataTests/Datasources/DogsRemoteDatasourceTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Network
 @testable import Data
 
 final class DogsRemoteDatasourceTests: XCTestCase {
@@ -23,5 +24,37 @@ final class DogsRemoteDatasourceTests: XCTestCase {
         let result = try XCTUnwrap(capturedResult.get())
         
         XCTAssertEqual(result, expectedResult)
+    }
+
+    func test_success_fetch_all_breeds_when_message_dictionary_is_empty() async throws {
+        // GIVEN
+        let expectedResult = AllBreedsResponseDTO(message: [:], status: "success")
+        let stub = HTTPCLientStub(result: .success(expectedResult.toData()))
+        let sut = DefaultDogsRemoteDatasource(httpClient: stub)
+
+        // WHEN
+        let capturedResult = await sut.fetchAllBreeds()
+
+        // THEN
+        let result = try XCTUnwrap(capturedResult.get())
+        
+        XCTAssertEqual(result, expectedResult)
+    }
+
+    func test_fail_fetch_all_breeds_when_client_fail() async throws {
+        // GIVEN
+        let expectedResult = RequestError.decode
+        let stub = HTTPCLientStub(result: .failure(expectedResult))
+        let sut = DefaultDogsRemoteDatasource(httpClient: stub)
+
+        // WHEN
+        let capturedResult = await sut.fetchAllBreeds()
+
+        // THEN
+        guard case .failure(let error) = capturedResult else {
+            return
+        }
+
+        XCTAssertEqual(error, expectedResult)
     }
 }
