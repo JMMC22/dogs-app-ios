670b0179c8d7f9d14cb880e51f8b681a9f973941
José María Márquez <jmmarquezcrespo@gmail.com>
2025-03-11 14:41:48 +0100

    initial commit

---
+++ DogsApp/DogsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DogsApp
+//
+//  Created by Márquez Crespo, José María on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DogsApp/DogsApp/DogsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsAppApp.swift
+//  DogsApp
+//
+//  Created by Márquez Crespo, José María on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DogsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
