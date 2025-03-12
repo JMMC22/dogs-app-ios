36f0842fb8a42d8845f5d9c5ef5142468097ed90
José María <jmmarquezcrespo@gmail.com>
2025-03-12 10:25:53 +0100

    Feature - List design (#9)
    * feat: add breed list row view to list
    
    * feat: add visual constants

---
+++ DogsApp/Packages/Features/Sources/Features/List/Components/BreedsListRowView.swift
@@ -0,0 +1,41 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Márquez Crespo, José María on 12/3/25.
+//
+
+import SwiftUI
+
+struct BreedsListRowView: View {
+
+    // MARK: Properties
+
+    private let name: String
+    private let action: () -> Void
+
+    init(name: String, action: @escaping () -> Void) {
+        self.name = name
+        self.action = action
+    }
+
+    // MARK: Visual constants
+
+    private enum VisualConstants {
+        static let spacing: CGFloat = 8
+    }
+
+    // MARK: Body
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: VisualConstants.spacing) {
+            Text(name.capitalized)
+            Divider()
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .contentShape(Rectangle())
+        .onTapGesture {
+            action()
+        }
+    }
+}
